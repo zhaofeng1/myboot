a187dc09c199328b43dc6d21bf475812a622d7d4
zhaofeng <zhaofeng.1100@163.com>
2018-03-12 11:08:31 +0800

    fix

--- myboot/boot-web/src/main/java/com/zf/web/control/CommonControl.java
+++ myboot/boot-web/src/main/java/com/zf/web/control/CommonControl.java
@@ -1,11 +1,15 @@
 package com.zf.web.control;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
 import com.zf.web.dao.UserRepository;
+import com.zf.web.model.User;
 
 @RestController
 @RequestMapping(value = "/common")
@@ -20,10 +24,17 @@ public class CommonControl {
 	@RequestMapping("/index")
 	public String home() {
 		log.info("index start!");
-		//		List<User> list = userRepository.findAll();
-		//		System.out.println(JSON.toJSONString(list));
+		List<User> list = userRepository.findAll();
+		System.out.println(JSON.toJSONString(list));
 		return "Hello World! index";
 		
 	}
+
+	//	@RequestMapping("/index")
+	//	public String home() {
+	//		log.info("index start!");
+	//		return "Hello World! index";
+	//		
+	//	}
 
 }
--- myboot/boot-web/src/test/java/com/zf/web/dao/UserResponsitoryTest.java
+++ myboot/boot-web/src/test/java/com/zf/web/dao/UserResponsitoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSON;
 import com.zf.web.model.User;
@@ -19,8 +20,34 @@ public class UserResponsitoryTest {
 	UserRepository userRepository;
 
 	@Test
-	public void test() {
+	public void findAllTest() {
 		List<User> list = userRepository.findAll();
 		System.out.println(JSON.toJSONString(list));
+	}
+
+	@Test
+	public void getByUsername() {
+		User user = userRepository.findByUsername("zf");
+		System.out.println(JSON.toJSONString(user));
+	}
+
+	@Test
+	public void saveTest() {
+		User user = new User();
+		user.setUsername("cs");
+		user.setUserpwd("123");
+
+		userRepository.save(user);
+	}
+
+	@Test
+	@Transactional(rollbackFor = Exception.class)
+	public void saveRollbackTest() throws Exception {
+		User user = new User();
+		user.setUsername("cs1");
+		user.setUserpwd("123");
+
+		userRepository.save(user);
+		//		throw new Exception("sysconfig error");
 	}
 }
