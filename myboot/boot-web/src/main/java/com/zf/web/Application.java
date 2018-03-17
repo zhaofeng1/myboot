def9bb70821e7a67ef5c5c0b374ccdd1b702779a
zhaofeng <zhaofeng.1100@163.com>
2018-03-17 15:54:04 +0800

    spring boot  jpa jsp 打成war包  直接运行。

--- myboot/boot-web/src/main/java/com/zf/web/Application.java
+++ myboot/boot-web/src/main/java/com/zf/web/Application.java
@@ -2,12 +2,13 @@ package com.zf.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 // @RestController
 // @EnableAutoConfiguration
 // @ComponentScan(basePackages = { "com.zf.web.*" })
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
---
+++ myboot/boot-web/src/main/java/com/zf/web/control/PageControl.java
@@ -0,0 +1,36 @@
+package com.zf.web.control;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.zf.web.dao.UserRepository;
+
+@Controller
+@RequestMapping(value = "/page")
+public class PageControl {
+
+	private static Logger log = Logger.getLogger(PageControl.class);
+
+	@Autowired
+	UserRepository userRepository;
+
+	//	@RequestMapping(value = "/index")
+	//	@RequestMapping("/index")
+
+	//	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	//	public ModelAndView toIndex() {
+	//		log.info("test start!");
+	//		return new ModelAndView("test");
+	//	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String toTest() {
+		log.info("test start!");
+		return "test";
+	}
+
+
+}
