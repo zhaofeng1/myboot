030e6f48955cd940db95b9bb849b415c88c22f13
zhaofeng <zhaofeng.1100@163.com>
2018-04-03 11:10:08 +0800

    test

---
+++ myboot/boot-web/src/test/java/com/zf/web/MyTest.java
@@ -0,0 +1,10 @@
+package com.zf.web;
+
+/**
+ * Created by dell on 2018/4/3.
+ */
+public class MyTest {
+    public static void mian(String[] args){
+        System.out.println("12312312");
+    }
+}
