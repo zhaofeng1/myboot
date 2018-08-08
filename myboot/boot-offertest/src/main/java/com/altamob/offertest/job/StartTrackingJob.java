dd0bdcfa4abf311e4e655f5656b2d6b39790031f
zhaofeng <zhaofeng.1100@163.com>
2018-08-08 18:08:20 +0800

    fix bug

--- myboot/boot-offertest/src/main/java/com/altamob/offertest/job/StartTrackingJob.java
+++ myboot/boot-offertest/src/main/java/com/altamob/offertest/job/StartTrackingJob.java
@@ -1,15 +1,34 @@
 package com.altamob.offertest.job;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.altamob.offertest.service.FeedOfferService;
+import com.altamob.offertest.tracking.OfferTracking;
 
 @Component
 @EnableScheduling
 public class StartTrackingJob {
 
-	@Scheduled(cron = "0 */1 * * * ? ")
+	private static Logger logger = Logger.getLogger(OfferTracking.class);
+
+	private static int threadnum = 10;
+
+	@Autowired
+	FeedOfferService feedOfferService;
+
+	@Scheduled(cron = "0 10 * * * ? ")
 	public void startTracking() {
-		System.out.println("job start");
+		logger.info("job start!");
+		List<Object[]> list = feedOfferService.getLastHourOffer();
+		if (list.size() > 0) {
+			OfferTracking.startTrackingFromDb(list, threadnum);
+		}
+		logger.info("job end!");
 	}
 }
